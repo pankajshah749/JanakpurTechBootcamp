a83a545f88ebae2e02c2f22f91637120a24c28e1
pankajshah749 <sahpankaj749@gmail.com>
2025-11-05 10:58:27 +0545

    Home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -90,10 +90,10 @@ function Home() {
             <div className="block">
               <div className="mt-8 flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
                 <a
-                  href="https://forms.gle/dRNJb6fAemTWpJeB8"
+                  href=""
                   className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
                 >
-                 Apply Now
+                 Application Closed
                 </a>
                   <div className="">
                 <Link to="/SponsorUS">
