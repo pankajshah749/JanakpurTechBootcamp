c5813af86575c559a6dcddd7908f100421a2af60
Anjali Gupta <guptaanjali2651@gmail.com>
2026-07-13 20:54:03 +0545

    fixing

--- src/Components/AISummit.jsx
+++ src/Components/AISummit.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SummitHero from "./Summit/Home";
-import Analytics from "./Summit/analytics";
+import Analytics from "./Summit/Analytics";
 import About from "./Summit/About";
 import WhyAttend from "./Summit/WhyAttend";
 import Agenda from "./Summit/Agenda";
@@ -11,7 +11,6 @@ const AISummit = () => {
     <main className="bg-white text-gray-900">
       <SummitHero />
       <Analytics />
-      {/* <About /> */}
       <WhyAttend />
       <Agenda />
       <Speakers />
